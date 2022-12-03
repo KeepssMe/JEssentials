bd45a8de8d8579df2c5701998ef15885c9df6355
Danila <KeepssMe@gmail.com>
2022-12-03 20:42:50 +0300

    Fix string utils

--- Projects/JEssentials.Base/src/ru/jessentials/utils/StringUtils.java
+++ Projects/JEssentials.Base/src/ru/jessentials/utils/StringUtils.java
@@ -2,12 +2,12 @@ package ru.jessentials.utils;
 
 public final class StringUtils {
 
-	public static boolean IsNotEmpty(String str) {
-		return IsNotEmpty(str, true); 
+	public static boolean IsNullOrEmpty(String str) {
+		return IsNullOrEmpty(str, true); 
 	}
 	
-	public static boolean IsNotEmpty(String str, boolean isBlank) {
-		return str != null && !(isBlank ? str.isBlank() : str.isEmpty());
+	public static boolean IsNullOrEmpty(String str, boolean isBlank) {
+		return str == null || (isBlank ? str.isBlank() : str.isEmpty());
 	}
 	
 }
--- Projects/Tests/src/ru/tests/MainTests.java
+++ Projects/Tests/src/ru/tests/MainTests.java
@@ -6,30 +6,34 @@ import ru.jessentials.utils.*;
 public class MainTests {
 
 	public static void main(String[] args) {
-		tupleTest();
+		strTest();
+		//tupleTest();
 	}
 
 	private static void strTest() {
-
 		var str1 = "";
 		var str2 = "   ";
 		var str3 = "hhhhh";
+		String str4 = null;
 		
-		System.out.println(StringUtils.IsNotEmpty(str1));
-		System.out.println(StringUtils.IsNotEmpty(str2));
-		System.out.println(StringUtils.IsNotEmpty(str3));
+		System.out.println(StringUtils.IsNullOrEmpty(str1));
+		System.out.println(StringUtils.IsNullOrEmpty(str2));
+		System.out.println(StringUtils.IsNullOrEmpty(str3));
+		System.out.println(StringUtils.IsNullOrEmpty(str4));
 		
 		System.out.println();
 
-		System.out.println(StringUtils.IsNotEmpty(str1, true));
-		System.out.println(StringUtils.IsNotEmpty(str2, true));
-		System.out.println(StringUtils.IsNotEmpty(str3, true));
+		System.out.println(StringUtils.IsNullOrEmpty(str1, true));
+		System.out.println(StringUtils.IsNullOrEmpty(str2, true));
+		System.out.println(StringUtils.IsNullOrEmpty(str3, true));
+		System.out.println(StringUtils.IsNullOrEmpty(str4, true));
 
 		System.out.println();
 		
-		System.out.println(StringUtils.IsNotEmpty(str1, false));
-		System.out.println(StringUtils.IsNotEmpty(str2, false));
-		System.out.println(StringUtils.IsNotEmpty(str3, false));
+		System.out.println(StringUtils.IsNullOrEmpty(str1, false));
+		System.out.println(StringUtils.IsNullOrEmpty(str2, false));
+		System.out.println(StringUtils.IsNullOrEmpty(str3, false));
+		System.out.println(StringUtils.IsNullOrEmpty(str4, false));
 	}
 	
 	private static void tupleTest() {
